5573535a412cdec2f37009dc3cd72fc4faf49816
zakariaBensmida <45175094+zakariaBensmida@users.noreply.github.com>
2025-06-28 01:35:40 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,13 @@
+import React from "react";
+import Dashboard from "./pages/Dashboard";
+import "./index.css";
 
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
