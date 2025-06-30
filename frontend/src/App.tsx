891ee642ab8582cc511c3062e857ad91c5ef7361
zakariaBensmida <45175094+zakariaBensmida@users.noreply.github.com>
2025-06-30 20:24:00 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import Dashboard from "./pages/Dashboard";
-import "./index.css";
+import React from 'react';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <div className="App">
+    <div className="min-h-screen bg-gray-50">
       <Dashboard />
     </div>
   );
