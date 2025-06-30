69b0f044f9ffebb1fe8f9144a99a9d423c1baaa4
zakariaBensmida <45175094+zakariaBensmida@users.noreply.github.com>
2025-06-30 20:23:31 +0200

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-Set-Content -Path C:\Users\zakar\Sindbad\frontend\src\main.tsx -Value @"
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -9,4 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>
 );
-"@
